b6e159db75055af8be22c9ba026d551bde0988f6
sunny <sunny@papapa.local>
2015-07-01 01:36:34 +0800

    初始化项目

---
+++ 01-新闻-YCC/01-新闻-YCC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-新闻-YCC
+//
+//  Created by sunny on 15/7/1.
+//  Copyright (c) 2015年 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
